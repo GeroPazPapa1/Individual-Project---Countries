4fcd88f473dd12b8cc0d4d8c54cf71116c1802e5
GeroPazPapa <geronimonicolaspazpapa@gmail.com>
2023-08-15 11:36:36 -0300

    terminado sin corregir errrores

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,33 +35,44 @@ function App() {
 export default App*/
 
 
-import './App.css'
+import './App.css'; // Importa el archivo de hoja de estilo './App.css'
 
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'; // Estas importaciones son parte de la librería react-router-dom, que es utilizada para gestionar la navegación en una aplicación de React mediante la definición de rutas y componentes correspondientes.
+//Route: Es un componente de enrutamiento que se utiliza para definir cómo se va a mostrar un componente específico cuando la URL coincide con una ruta determinada.
+//Routes: Es un componente que contiene las definiciones de las rutas y los componentes correspondientes.
+//useLocation: Es un hook que se utiliza para obtener información sobre la ubicación actual de la URL. Proporciona datos sobre la ruta actual y otros detalles de la ubicación.
 
-import CreateActivity from './components/CreateActivity/CreateActivity'
-import Detail from './components/Detail/Detail'
-import HomePage from './components/HomePage/HomePage'
-import LandingPage from './components/LandingPage/LandingPage'
-import Nav from './components/Nav/Nav'
-import ResultsSearch from './components/SearchBar/ResultsSearch'
 
+
+import CreateActivity from './components/CreateActivity/CreateActivity'; // Importa el componente CreateActivity desde el archivo CreateActivity.jsx. Esto permite utilizar y renderizar el componente en la aplicación.
+import Detail from './components/Detail/Detail'; // Importa el componente Detail desde el archivo Detail.jsx. Al igual que en el caso anterior, esto permite utilizar y renderizar el componente en la aplicación.
+import HomePage from './components/HomePage/HomePage'; // Importa el componente HomePage desde el archivo HomePage.jsx. De manera similar, esto permite utilizar y renderizar el componente en la aplicación.
+import LandingPage from './components/LandingPage/LandingPage'; // Importa el componente LandingPage desde el archivo LandingPage.jsx. Esto permite utilizar y renderizar el componente en la aplicación.
+import Nav from './components/Nav/Nav'; //  Importa el componente Nav desde el archivo Nav.jsx. Este componente probablemente representa la barra de navegación de la aplicación, lo que permite navegar entre diferentes secciones de la aplicación.
+import ResultsSearch from './components/SearchBar/ResultsSearch'; // Importa el componente ResultsSearch desde el archivo ResultsSearch.jsx. Este componente probablemente se utiliza para mostrar los resultados de una búsqueda en la aplicación.
+
+// Define la función componente App. Esta es la parte principal del archivo y define
+// cómo se organizarán las diferentes páginas y componentes en la aplicación.
 function App() {
   
-  const location = useLocation()
+  const location = useLocation(); //  Utiliza el hook useLocation para obtener la ubicación actual de la URL. Esto se utiliza para determinar si se debe mostrar el componente de navegación (Nav) según la ubicación actual.
   
-  return (
-    <div className='Container'>
-        {location.pathname!=='/' && <Nav/>}
-        <Routes>
-          <Route path='/' element={<LandingPage/>}/>
-          <Route path='/home' element={<HomePage/>}/>
-          <Route path='/result' element={<ResultsSearch/>}/>
-          <Route path='/detail/:id' element={<Detail/>}/>
-          <Route path='/create' element={<CreateActivity/>}/>
+  return ( // El componente App devuelve el contenido JSX que representa la estructura general de la aplicación.
+    <div className='Container'> {/* Un contenedor principal con la clase CSS 'Container' que envuelve todo el contenido de la aplicación.*/}
+        {location.pathname!=='/' && <Nav/>} {/* Una condición que verifica si la ubicación actual no es la página de inicio ('/'). Si es cierto, se renderiza el componente Nav.*/}
+        <Routes> {/* Utiliza el componente Routes para definir las rutas y los componentes correspondientes que deben renderizarse cuando se accede a esas rutas.*/}
+          <Route path='/' element={<LandingPage/>}/> {/*  Define una ruta que coincide con la URL raíz ('/') y renderiza el componente LandingPage.*/}
+          <Route path='/home' element={<HomePage/>}/> {/* Define una ruta que coincide con la URL '/home' y renderiza el componente HomePage.*/}
+          <Route path='/result' element={<ResultsSearch/>}/> {/* Define una ruta que coincide con la URL '/result' y renderiza el componente ResultsSearch.*/}
+          <Route path='/detail/:id' element={<Detail/>}/> {/* Define una ruta con un parámetro dinámico :id que renderiza el componente Detail.*/}
+          <Route path='/create' element={<CreateActivity/>}/> {/* Define una ruta que coincide con la URL '/create' y renderiza el componente CreateActivity.*/}
         </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+// Exporta el componente App para que pueda ser utilizado por otros componentes y módulos de la aplicación.
+
+// En resumen, este archivo es la raíz de tu aplicación y define cómo se organizan las diferentes páginas y componentes en función de las rutas especificadas. También gestiona la visibilidad del componente de navegación (Nav) y renderiza el componente correspondiente según la ruta actual.
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,19 +9,24 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )*/
 
-import './index.css'
+import './index.css'; //  Esta línea importa el archivo de estilos index.css, que contiene estilos globales para la aplicación.
 
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import store from './redux/store/store'
+import App from './App.jsx'; // Importa el componente App desde el archivo App.jsx. El componente App es el componente principal de la aplicación y define la estructura y las rutas de la misma.
+import { BrowserRouter } from 'react-router-dom'; // Importa el componente BrowserRouter de la librería react-router-dom. El BrowserRouter proporciona el enrutamiento y gestiona la historia del navegador, permitiendo que la aplicación cambie de contenido según la URL actual.
+import { Provider } from 'react-redux'; // Importa el componente Provider de la librería react-redux. El Provider es un componente de nivel superior que permite que el store de Redux esté disponible para todos los componentes de la aplicación.
+import React from 'react'; // Importa el módulo React, que es necesario para trabajar con componentes de React.
+import ReactDOM from 'react-dom/client'; // Importa el módulo ReactDOM para renderizar los componentes de React en el DOM.
+import store from './redux/store/store'; // Importa el store de Redux desde el archivo store.js en la carpeta store dentro de la carpeta redux. Este store centraliza el estado de la aplicación y las acciones que modifican ese estado.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+// crea un "root" en el DOM, que es el punto de entrada para renderizar componentes de React. El parámetro que se pasa a esta función es el elemento en el DOM en el que deseas que se renderice la aplicación. En este caso, se utiliza el elemento con el id 'root'
+//  Este método se llama en el root creado en el paso anterior para iniciar el proceso de renderización. Lo que viene después del paréntesis es lo que se va a renderizar en el root.
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
+{/* El componente Provider es proporcionado por react-redux y se utiliza para envolver la aplicación con el store de Redux. Esto permite que todos los componentes de la aplicación puedan acceder al estado global de Redux y a las acciones para modificarlo.*/}
+{/* El prop store={store} se utiliza para proporcionar el store creado en el archivo store.js.*/}
+    <BrowserRouter> {/* Este componente de react-router-dom envuelve la aplicación y le proporciona las capacidades de enrutamiento. Permite que la aplicación responda a cambios en la URL y renderice los componentes correspondientes según la ruta actual.*/}
+      <App /> {/* Esto es el componente App, que es el componente principal de la aplicación. Contiene las rutas y estructura general de la interfaz.*/}
     </BrowserRouter>
   </Provider>
-)
+);
+// En resumen, esta parte del código crea un root en el DOM, configura el enrutamiento y el estado global de Redux para la aplicación y finalmente renderiza el componente App dentro de este contexto configurado. Esto establece la base para toda la aplicación y permite que se muestren los componentes apropiados según la URL y el estado de Redux.
